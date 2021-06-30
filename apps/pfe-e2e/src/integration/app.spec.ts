4aee19ca2f9a4e58cd3ef8f2dca59cdf0ac49624
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2021-06-30 20:25:18 +0530

    updated complete application walk through with cypress e2e test

--- apps/pfe-e2e/src/integration/app.spec.ts
+++ apps/pfe-e2e/src/integration/app.spec.ts
@@ -1,15 +1,34 @@
 
 describe('pfe', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.fixture('transactions.json').as('transJSON');
+    cy.request("https://test-demo-6dd40-default-rtdb.firebaseio.com/transactions.json").then(res => {
+      expect(res.status).to.eq(200)
+    });
+  });
 
   it('should display welcome message', () => {
+
     cy.visit('/');
-    cy.contains('Frontend Technical Assignment')
+    cy.get(".transaction-item").should("have.length", 11)
+
   });
   it('should transfer money', () => {
     cy.get('#to-account').type('Ellit')
     cy.get('#amount').type('2500')
     cy.get('.btn-primary').click();
     cy.get('.background-brown-dark').click();
+    cy.get(".transaction-item").should("have.length", 12)
+  })
+
+  it('should filter the history list by el letters', () => {
+    cy.get('#transactions').type('el')
+    cy.get(".transaction-item").should("have.length", 3)
+  })
+
+  it('should filter the history list by whole letters', () => {
+    cy.get('#transactions').clear();
+    cy.get('#transactions').type('whole')
+    cy.get(".transaction-item").should("have.length", 1)
   })
 });
